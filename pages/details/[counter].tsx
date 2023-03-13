4ab44d4db5a4f9de3111869f7950e2db380f7e95
Paul LOPEZ <p.lopez@alienor.net>
2023-03-13 20:54:23 +0100

    [LAYOUT] fix taille logo page détail

--- pages/details/[counter].tsx
+++ pages/details/[counter].tsx
@@ -82,7 +82,7 @@ export default function Counters({ details, buildTime }: Props) {
       <div className="p-4">
         <Link href="https://velo-cite.org">
           <img
-            className="float-left w-20 cursor-pointer"
+            className="float-left h-20 cursor-pointer mr-5"
             src="/logo.png"
             alt="Logo Vélo-Cité Bordeaux"
           />
