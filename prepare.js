3d292269856afe656f8601923fa3e2944820a976
CyclisteBx <sped-seldom-0z@icloud.com>
2026-01-22 14:21:31 +0100

    feat: ajout compteurs quai des Queryes

--- prepare.js
+++ prepare.js
@@ -173,7 +173,7 @@ const prepare = (ids, details, metadata, counter) => {
       count: 0,
     };
   const now = DateTime.local().set(dayFmt);
-  const oneDay = now.minus({ day: 2 }).toISO();
+  const oneDay = now.minus({ day: 3 }).toISO();
   const oneMonth = now.minus({ month: 1 }).toISO();
   const twoYear = now.minus({ year: 2 }).toISO();
 
