feb37cee2c2a0406c93935c413c4fb8685143504
CyclisteBx <sped-seldom-0z@icloud.com>
2026-01-22 14:42:15 +0100

    cleanup: revert 3 files incorrectly updated

--- scripts/rename-header-compteurs-csv.sh
+++ scripts/rename-header-compteurs-csv.sh
@@ -1,8 +1,8 @@
 # suppression caratère de l'UTF8 with BOM
-sed -i '' '1s/^\xEF\xBB\xBF//' public/compteurs.csv
+sed -i '1s/^\xEF\xBB\xBF//' public/compteurs.csv
 
-sed -i '' "s/ident/id_compteur/" public/compteurs.csv
-sed -i '' "s/comptage_1h/sum_counts/" public/compteurs.csv
+sed -i "s/ident/id_compteur/" public/compteurs.csv
+sed -i "s/comptage_1h/sum_counts/" public/compteurs.csv
 # ajout d'une colonne pour que la date ne soit pas la première et soit correctemment traitée par le js
 # sed -i "s/^/p;/" public/compteurs.csv
-sed -i '' "s/datedebut;/date;/" public/compteurs.csv
+sed -i "s/datedebut;/date;/" public/compteurs.csv
