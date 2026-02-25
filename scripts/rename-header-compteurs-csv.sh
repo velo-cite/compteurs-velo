98298a707b15c771996be92317efdafd2fa9e708
CyclisteBx <sped-seldom-0z@icloud.com>
2026-02-25 13:42:52 +0100

    Ajout compteur rue Georges Bonnac

--- scripts/rename-header-compteurs-csv.sh
+++ scripts/rename-header-compteurs-csv.sh
@@ -6,3 +6,5 @@ sed -i "s/comptage_1h/sum_counts/" public/compteurs.csv
 # ajout d'une colonne pour que la date ne soit pas la première et soit correctemment traitée par le js
 # sed -i "s/^/p;/" public/compteurs.csv
 sed -i "s/datedebut;/date;/" public/compteurs.csv
+sed -i "s/rue Georges Bonnac;Boulevard vers Bordeaux Centre;vers Bordeaux Centre/rue Georges Bonnac;rue Georges Bonnac vers Bordeaux Centre;vers Bordeaux Centre/g" public/compteurs.csv
+sed -i "s/rue Georges Bonnac;Bordeaux Centre vers Boulevard;vers Boulevard/rue Georges Bonnac;rue Georges Bonnac vers Boulevard;vers Boulevard/g" public/compteurs.csv
